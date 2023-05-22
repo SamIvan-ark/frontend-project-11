e86cd347430d4d3614c9ae01e1c7f5b07d575efc
Ivan Samozhenov <i_dont_known@mail.ru>
2023-05-22 21:28:14 +0300

    Add rules to Webpack that allow not to specify .js in imports

--- webpack.config.js
+++ webpack.config.js
@@ -12,17 +12,19 @@ export default {
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: ['@babel/preset-env'],
-      //     },
-      //   },
-      // },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
