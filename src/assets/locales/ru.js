2cae5ce8670f959e13ff0f99259c7ceb97af99e1
Ivan Samozhenov <i_dont_known@mail.ru>
2023-05-22 21:25:23 +0300

    Delete unnecessary texts in i18n

--- src/assets/locales/ru.js
+++ src/assets/locales/ru.js
@@ -1,22 +1,16 @@
 export default {
   translation: {
-    header: 'RSS агрегатор',
-    description: 'Начните читать RSS сегодня! Это легко, это красиво',
     form: {
-      inputPlaceholder: 'Ссылка RSS',
-      button: 'Добавить',
       messages: {
         errors: {
           invalid: 'Ссылка должна быть валидным URL',
           nonUnique: 'RSS уже существует',
           noRss: 'Ресурс не содержит валидный RSS',
-          empty: 'Не должно быть пустым',
           network: 'Ошибка сети',
         },
         success: 'RSS успешно загружен',
       },
     },
-    example: 'Пример: https://ru.hexlet.io/lessons.rss',
     content: {
       posts: {
         title: 'Посты',
