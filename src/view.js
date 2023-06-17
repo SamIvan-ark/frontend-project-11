b5e8e8a587b72e7b75c63426e74282f27c556214
Ivan Samozhenov <i_dont_known@mail.ru>
2023-06-17 19:30:36 +0300

    Fix scroll lock when modal is open, improve naming in view.js (posts)

--- src/view.js
+++ src/view.js
@@ -100,7 +100,7 @@ const renderModal = (elements, modalStatus) => {
       break;
     case 'shown':
       body.classList.add('modal-open');
-      body.setAttribute('style', 'overflow: hidden, padding-right: 19px');
+      body.setAttribute('style', 'overflow: hidden; padding-right: 19px;');
       backdrop.classList.add('modal-backdrop', 'show');
       modal.classList.add('show');
       modal.removeAttribute('aria-hidden');
@@ -126,11 +126,12 @@ const renderModalContent = (elements, id, state) => {
   readMoreButton.setAttribute('href', link);
 };
 
-const renderViewedPosts = (elements, list) => {
-  const { posts } = elements;
-  const renderedPosts = posts.getElementsByTagName('LI');
+const renderViewedPosts = (elements, state) => {
+  const { ui: { viewedPosts: list } } = state;
+  const { posts: postsWrapper } = elements;
+  const renderedPosts = postsWrapper.getElementsByTagName('LI');
   const viewedPosts = Array.from(renderedPosts).filter((post) => {
-    const currentId = Number(post.querySelector('A').dataset.id);
+    const currentId = Number(post.querySelector('A').dataset.postId);
     return list.includes(currentId);
   });
 
@@ -159,17 +160,17 @@ const renderPosts = (elements, state, i18n) => {
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
     const a = document.createElement('A');
     a.classList.add('fw-bold');
-    a.dataset.id = id;
+    a.dataset.postId = id;
     a.setAttribute('href', link);
     a.setAttribute('target', '_blank');
     a.setAttribute('rel', 'noopener noreffer');
-    a.dataset.type = 'link';
+    a.dataset.markAsVisited = true;
     a.textContent = title;
 
     const button = document.createElement('BUTTON');
-    button.setAttribute('type', 'button');
-    button.dataset.id = id;
-    button.dataset.type = 'button';
+    button.dataset.postId = id;
+    button.dataset.markAsVisited = true;
+    button.dataset.action = 'openModal';
     button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
     button.textContent = i18n('content.posts.button');
 
